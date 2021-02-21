9c36941564a96768c0ad8360e3cd547ccde10066
Davy <davycarvalho2017@hotmail.com>
2021-02-21 18:10:01 -0300

    atualizações de responsividade mobile

--- projeto_findpet/core/static/js/navbar_fixed.js
+++ projeto_findpet/core/static/js/navbar_fixed.js
@@ -1,12 +1,12 @@
-window.onscroll = function() {myFunction()};
+// window.onscroll = function() {myFunction()};
 
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
+// var navbar = document.getElementById("navbar");
+// var sticky = navbar.offsetTop;
 
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
+// function myFunction() {
+//   if (window.pageYOffset >= sticky) {
+//     navbar.classList.add("sticky")
+//   } else {
+//     navbar.classList.remove("sticky");
+//   }
+// }
